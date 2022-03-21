5d5141164d72bc41de397f63091351864b76555f
samuelsholib <samuelsholib12@gmail.com>
2022-03-20 20:33:55 -0700

    server reads html from node.js

--- db/schema.sql
+++
@@ -1,2 +0,0 @@
-DROP DATABASE IF EXISTS ecom_db;
-CREATE DATABASE ecom_db;
--- server.js
+++ server.js
@@ -1,44 +1,18 @@
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const routes = require('./controllers');
-const helpers = require('./utils/helpers');
+const http = require('http');
+const fs = require('fs')
 
-const sequelize = require('./config/connection');
+function readHtml(request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.readFile('./index.html', null, function (error, data) {
+        if (error) {
+            response.writeHead(404);
+            response.write('File not found')
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+}
+http.createServer(readHtml).listen(8000);
+console.log('Now listening @ http://localhost:8000/')
 
-// Create a new sequelize store using the express-session package
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-const hbs = exphbs.create({ helpers });
-
-// Configure and link a session object with the sequelize store
-const sess = {
-  secret: 'Super secret secret',
-  cookie: {
-  },
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-};
-
-// Add express-session and store as Express.js middleware
-app.use(session(sess));
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(routes);
-
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
-});
